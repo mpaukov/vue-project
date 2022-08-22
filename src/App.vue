411ed68c50603d254e0d125d1aba8ef6c5335615
Michael Paukov <michael.paukov@gmail.com>
2022-08-22 15:02:20 +0200

    practice

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ApartmentItem />
+</template>
+
+
+
+    <script>
+import ApartmentItem from './components/apartment/ApartmentItem.vue';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    ApartmentItem
+  },
+
+}
+</script>
+
+    <style>
+    #app {
+      font-family: Montserrat, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+    </style>
